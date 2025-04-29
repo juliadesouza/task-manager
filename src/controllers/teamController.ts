d4ace5c1e9e8a0f0fd5cf90d14970bb51923774b
Julia Souza <juliasouza@Julias-MacBook-Pro.local>
2025-04-29 19:36:10 -0300

    feat: added insertion and deletion of a team member

--- src/controllers/teamController.ts
+++ src/controllers/teamController.ts
@@ -40,6 +40,46 @@ class TeamController {
     await database.team.update({ where: { id }, data: { description } });
     response.json({});
   }
+
+  async createTeamMember(request: Request, response: Response): Promise<void> {
+    const schema = z.object({
+      userId: z.string().uuid(),
+      teamId: z.string().uuid(),
+    });
+
+    const { userId, teamId } = schema.parse(request.body);
+
+    const doesUserExist = await database.user.findFirst({
+      where: { id: userId },
+    });
+
+    if (!doesUserExist) {
+      throw new AppError("User not found.", 404);
+    }
+
+    const doesTeamExist = await database.team.findFirst({
+      where: { id: teamId },
+    });
+
+    if (!doesTeamExist) {
+      throw new AppError("Team not found.", 404);
+    }
+
+    await database.teamMember.create({ data: { userId, teamId } });
+
+    response.status(201).json({ userId, teamId });
+  }
+
+  async deleteTeamMember(request: Request, response: Response): Promise<void> {
+    const schema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = schema.parse(request.params);
+
+    await database.teamMember.delete({ where: { id } });
+    response.status(204).json({});
+  }
 }
 
 export { TeamController };
